c9613186f70cdb6bd4ecc00cc9f0e97a793b9335
Sharath Babu <sharath.ise@gmail.com>
2024-02-14 12:16:57 +0530

    Added javadeploy.sh

---
+++ javadeploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+status=`ps -ef | grep telspiel | grep jar | awk '{print $2}'`
+
+if [ -z "$status" ];
+        then
+        echo "Application is NOT running & we're starting now...";
+        chmod 755 $WORKSPACE/target/telspiel-0.0.1-SNAPSHOT.war;
+        nohup java -war $WORKSPACE/target/telspiel-0.0.1-SNAPSHOT.war &
+        echo "Done..";
+        if [[ "$?" == 0 ]]; then echo "Application has been started successfully with PID :"; echo `ps -ef | grep telspiel | grep jar | awk '{print $2}'`;
+        else echo "Application deployment failed"; exit 1; fi
+
+else
+        echo "Application is already running & hence killing previous pid :";
+        kill -9 $status;
+        if [[ "$?" == 0 ]]; then echo "Previous instance of the Application has been stopped successfully";
+                else echo "Failed to stop previous instance of the Application"; exit 1;
+        fi
+        chmod 755 $WORKSPACE/telspiel-0.0.1-SNAPSHOT.war;
+        nohup java -war $WORKSPACE/target/telspiel-0.0.1-SNAPSHOT.war &
+        echo "Done..";
+        if [[ "$?" == 0 ]]; then echo "Application has been deployed successfully with PID :"; echo `ps -ef | grep telspiel | grep jar | awk '{print $2}'`;
+        else echo "Application deployment failed"; exit 1; fi
+fi
+
+exit 0
